7452f40f806c042e8132a1b1ac19718081dd2cab
chaeyeonwoo <codusdn0210@gmail.com>
2025-06-04 23:42:03 +0900

    실행 성공

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,38 +1,34 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android") version "2.0.0"
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.qna_frontend"
-    compileSdk = flutter.compileSdkVersion
-    // ndkVersion = flutter.ndkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
-    defaultConfig {
-        applicationId = "com.example.qna_frontend"
-        minSdk = 23 // ✅ Firebase가 요구하는 최소 SDK 버전
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.qna_frontend"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -43,9 +39,6 @@ flutter {
 }
 
 dependencies {
-    // Import the Firebase BoM (Bill of Materials)
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
-
-    // Firebase Analytics (you can add more Firebase services here)
     implementation("com.google.firebase:firebase-analytics")
 }
